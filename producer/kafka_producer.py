82b8823902c6c5fd563c278b8e00faa74f57bcc9
kaenyyh <kaenyyh@gmail.com>
2018-05-09 14:54:16 -0700

    web ui

--- producer/kafka_producer.py
+++ producer/kafka_producer.py
@@ -1,19 +1,22 @@
 from kafka import KafkaProducer, KafkaConsumer
 import time, sys
-from utils_function import parse_header
 from time import gmtime, strftime
 
 
 def main():
-    producer = KafkaProducer(bootstrap_servers='ec2-34-213-54-16.us-west-2.compute.amazonaws.com:9092', key_serializer=str.encode, value_serializer=str.encode)
+    producer = KafkaProducer(bootstrap_servers='ec2-50-112-36-122.us-west-2.compute.amazonaws.com:9092')
+    
+    file_address = "/home/ubuntu/input.txt"
 
     count = 0
-    with open(input.txt) as f:
+    with open(file_address) as f:
         for line in f:
             curtime = strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-            new_line = line + curtime
-            producer.send("ctest", key=str(count), value=new_line)
+            new_line = line + " " + curtime
+            producer.send("ctest", new_line)
             count += 1
+	    print("now it is sending... " + new_line)
+    producer.flush()
     f.close()
 
 if __name__ == "__main__":
